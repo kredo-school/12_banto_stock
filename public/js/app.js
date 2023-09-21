0c783388148541cf3a8feccc57ed4685dfb90f6e
yuya1453 <yuya19991@outlook.jp>
2023-09-21 20:24:11 +0900

    I haven't touched it, but it came up in the commit changes. I don't know how to deal with it, so I would like to ask the nucleus to help me.

--- public/js/app.js
+++ public/js/app.js
@@ -995,16 +995,6 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
 
 /***/ }),
 
-/***/ "./resources/sass/ItemEdit.scss":
-/*!**************************************!*\
-  !*** ./resources/sass/ItemEdit.scss ***!
-  \**************************************/
-/***/ (() => {
-
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/css-loader/dist/cjs.js):\nError: Can't resolve '../images/carrot.jpg' in 'C:\\Users\\yuyu\\Desktop\\banto_stock\\resources\\sass'\n    at finishWithoutResolve (C:\\Users\\yuyu\\Desktop\\banto_stock\\node_modules\\enhanced-resolve\\lib\\Resolver.js:369:18)\n    at C:\\Users\\yuyu\\Desktop\\banto_stock\\node_modules\\enhanced-resolve\\lib\\Resolver.js:461:15\n    at C:\\Users\\yuyu\\Desktop\\banto_stock\\node_modules\\enhanced-resolve\\lib\\Resolver.js:519:5\n    at eval (eval at create (C:\\Users\\yuyu\\Desktop\\banto_stock\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:16:1)\n    at C:\\Users\\yuyu\\Desktop\\banto_stock\\node_modules\\enhanced-resolve\\lib\\Resolver.js:519:5\n    at eval (eval at create (C:\\Users\\yuyu\\Desktop\\banto_stock\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:27:1)\n    at C:\\Users\\yuyu\\Desktop\\banto_stock\\node_modules\\enhanced-resolve\\lib\\DescriptionFilePlugin.js:89:43\n    at C:\\Users\\yuyu\\Desktop\\banto_stock\\node_modules\\enhanced-resolve\\lib\\Resolver.js:519:5\n    at eval (eval at create (C:\\Users\\yuyu\\Desktop\\banto_stock\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:15:1)\n    at C:\\Users\\yuyu\\Desktop\\banto_stock\\node_modules\\enhanced-resolve\\lib\\Resolver.js:519:5\n    at processResult (C:\\Users\\yuyu\\Desktop\\banto_stock\\node_modules\\webpack\\lib\\NormalModule.js:764:19)\n    at C:\\Users\\yuyu\\Desktop\\banto_stock\\node_modules\\webpack\\lib\\NormalModule.js:866:5\n    at C:\\Users\\yuyu\\Desktop\\banto_stock\\node_modules\\loader-runner\\lib\\LoaderRunner.js:400:11\n    at C:\\Users\\yuyu\\Desktop\\banto_stock\\node_modules\\loader-runner\\lib\\LoaderRunner.js:252:18\n    at context.callback (C:\\Users\\yuyu\\Desktop\\banto_stock\\node_modules\\loader-runner\\lib\\LoaderRunner.js:124:13)\n    at Object.loader (C:\\Users\\yuyu\\Desktop\\banto_stock\\node_modules\\css-loader\\dist\\index.js:155:5)\n    at process.processTicksAndRejections (node:internal/process/task_queues:95:5)");
-
-/***/ }),
-
 /***/ "./node_modules/process/browser.js":
 /*!*****************************************!*\
   !*** ./node_modules/process/browser.js ***!
@@ -1200,8 +1190,7 @@ module.exports = JSON.parse('{"name":"axios","version":"0.21.4","description":"P
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
 /******/ 	__webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/js/app.js")))
-/******/ 	__webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/sass/app.scss")))
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/sass/ItemEdit.scss")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/sass/app.scss")))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
