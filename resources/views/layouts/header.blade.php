c9d174bdb120a38029742acc75b4e3513d0d0ebb
hikaritanaka1695 <hikari.kuredo.1965@gmail.com>
2023-09-22 21:35:41 +0900

    I added it, please check

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -32,9 +32,15 @@
               <li class="nav-item">
                 <a class="nav-link" href="#"><i class="fi fi-sr-ballot" style="margin-right: 5px;"></i>Transaction</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#"><i class="fi fi-sr-exit" style="margin-right: 5px;"></i>log out</a>
-              </li>
+              <a class="dropdown-item" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+              </a>
+
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                  @csrf
+              </form>
             </ul>
           </div>
         </div>
