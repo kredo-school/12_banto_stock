6edbcefc3abcc7bf60914e3bf5ff70dd1c98abc8
hikaritanaka1695 <hikari.kuredo.1965@gmail.com>
2023-10-16 00:36:50 +0900

    The conflict has been fixed

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,18 +4,3 @@ const { VueLoaderPlugin } = require('vue-loader');
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
    .version();
-
-mix.webpackConfig({
-   module: {
-       rules: [
-           {
-               test: /\.vue$/,
-               loader: 'vue-loader'
-           },
-           // 他のルールもここに追加できます
-       ]
-   },
-   plugins: [
-       new VueLoaderPlugin()
-   ]
-});
